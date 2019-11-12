a5af885d998b99aecbda36cfd53d94520e4452c1
Mathilde Robert <m.robert@skema.edu>
2019-11-12 20:34:33 +0100

    rails wikinimous

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @articles = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+
+  end
 end
